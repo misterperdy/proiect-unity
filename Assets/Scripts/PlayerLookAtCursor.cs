729f8ca970bc9d4771bcf13173011974ad7ec98b
ravegoth <trxixn@gmail.com>
2025-11-03 20:45:48 +0200

    feat: Implement smooth player rotation towards cursor

---
+++ Assets/Scripts/PlayerLookAtCursor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerLookAtCursor : MonoBehaviour
+{
+    public float rotationSpeed = 5f;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
