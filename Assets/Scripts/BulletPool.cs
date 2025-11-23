b0ecbf175c5d8c76a0b7ff56356f18ff34dabb23
Antonie Matei <antonie.matei@agvr.co>
2025-11-23 22:09:37 +0200

    Add Bullet Pooling on player arrows

---
+++ Assets/Scripts/BulletPool.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+//manager script for bullet/arrow pool
+
+public class BulletPool : MonoBehaviour
+{
+    public static BulletPool Instance; // singleton 
+
+    public GameObject arrowPrefab;
+    public int defaultCapacity = 20;
+    public int maxCapacity = 100;
+
+    private ObjectPool<GameObject> _pool;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        //initialize the pool
+        _pool = new ObjectPool<GameObject>(
+            createFunc: () => Instantiate(arrowPrefab),
+            actionOnGet: (obj) => obj.SetActive(true),
+            actionOnRelease: (obj) => obj.SetActive(false),
+            actionOnDestroy: (obj) => Destroy(obj),
+            collectionCheck: false,
+            defaultCapacity: defaultCapacity,
+            maxSize: maxCapacity
+            );
+    }
+
+    //To get new bulllet
+    public GameObject GetBullet()
+    {
+        return _pool.Get();
+    }
+
+    //optional,can be done directly with SetActive(false)
+    public void ReturnBullet(GameObject bullet)
+    {
+        _pool.Release(bullet);
+    }
+}
