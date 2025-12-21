f8350f32261417755239070c20563f46eccde57a
Antonie Matei <antonie.matei@agvr.co>
2025-12-21 13:06:18 +0200

    trying to fix jittery player rotation

--- Assets/Scripts/Player/PlayerLookAtCursor.cs
+++ Assets/Scripts/Player/PlayerLookAtCursor.cs
@@ -1,17 +1,42 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerLookAtCursor : MonoBehaviour
 {
     public float rotationSpeed = 5f;
 
+    private Rigidbody rb;
+    private Camera cam;
+
+    private Vector3 targetDirection;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+    }
+
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            Vector3 targetPosition = hit.point;
+            targetPosition.y = transform.position.y;
+
+            targetDirection = targetPosition - transform.position;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //apply physics here, rotate Rigidbody instead of transform
+        if(targetDirection!= Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            Quaternion nextRotation = Quaternion.Lerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+
+            rb.MoveRotation(nextRotation);
         }
     }
 }
