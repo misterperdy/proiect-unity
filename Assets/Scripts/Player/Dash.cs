0acb8f06068877c040e103a5a29ac977de16e159
Antonie Matei <antonie.matei@agvr.co>
2025-12-21 19:29:00 +0200

    dash animation

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -8,10 +8,10 @@ public class Dash : MonoBehaviour
     private Rigidbody _rigidBody;
     private TrailRenderer _trailRenderer;
     private PlayerHealth _playerHealth;
-
+    private Animator animator;
 
     [Header("Dashing")]
-    [SerializeField] private float _dashVelocity = 1.2f;
+    [SerializeField] private float _dashVelocity = 15f; // Am marit putin valoarea, 1.2 e foarte mic pt MovePosition
     [SerializeField] private float _dashingTime = 0.2f;
     [SerializeField] private float _dashCooldown = 1f;
     private Vector3 _dashingDir;
@@ -19,60 +19,85 @@ public class Dash : MonoBehaviour
     private bool _canDash = true;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
         _trailRenderer = GetComponent<TrailRenderer>();
         _playerHealth = GetComponent<PlayerHealth>();
-
-
+        // Atentie: Daca scriptul e pe parinte si animatorul pe copil, foloseste GetComponentInChildren
+        animator = GetComponentInChildren<Animator>();
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         if (PauseManager.IsPaused) return;
 
         var dashInput = Input.GetButtonDown("Dash");
 
+        // 1. Verificam Input-ul
         if (dashInput && _canDash)
         {
-            
-            _isDashing = true;
-            _canDash = false;
-            _trailRenderer.emitting = true;
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            _dashingDir = new Vector3(horizontal, 0, vertical);
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
 
+            _dashingDir = new Vector3(x, 0, z).normalized;
+
+            // OPTIONAL: Daca nu apasa nimic, dam dash in fata (transform.forward)
+            // Sau daca vrei sa nu faca nimic daca nu apasa, lasi un return aici.
             if (_dashingDir == Vector3.zero)
             {
-                _canDash = false;
+                // Varianta A: Dash in fata daca sta pe loc
+                _dashingDir = transform.forward;
+
+                // Varianta B (cea din codul tau vechi): Nu face dash
+                // return; 
             }
 
-            StartCoroutine(StopDashing());
-        }
+            // Setam parametrii pentru Blend Tree ca sa stie animatia directia
+            animator.SetFloat("InputX", x);
+            animator.SetFloat("InputZ", z);
 
-        if (_isDashing)
-        {
-            Physics.IgnoreLayerCollision(7, 8, true);
-            _playerHealth.canTakeDamage = false;
-
-            _rigidBody.MovePosition(_rigidBody.position + _dashingDir.normalized * _dashVelocity * Time.fixedDeltaTime);
-            return;
+            StartCoroutine(PerformDash());
         }
     }
 
-    private IEnumerator StopDashing()
+    // Folosim FixedUpdate pentru miscarea fizica (Rigidbody)
+    void FixedUpdate()
     {
+        if (_isDashing)
+        {
+            // Aici mutam efectiv jucatorul
+            _rigidBody.MovePosition(_rigidBody.position + _dashingDir * _dashVelocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private IEnumerator PerformDash()
+    {
+        _canDash = false;
+        _isDashing = true;
+
+        // AICI ESTE FIX-UL: Declansam animatia o singura data, la inceput
+        animator.SetTrigger("t_dash");
+
+        _trailRenderer.emitting = true;
+
+        // Dezactivam coliziunile si damage-ul
+        Physics.IgnoreLayerCollision(7, 8, true);
+        if (_playerHealth != null) _playerHealth.canTakeDamage = false;
+
+        // Asteptam cat dureaza dash-ul (timp in care FixedUpdate il muta)
         yield return new WaitForSeconds(_dashingTime);
-        Physics.IgnoreLayerCollision(7, 8, false);
-        _playerHealth.canTakeDamage = true;
+
+        // Oprim Dash-ul
         _isDashing = false;
         _trailRenderer.emitting = false;
-        _rigidBody.velocity = Vector3.zero;
+        _rigidBody.velocity = Vector3.zero; // Oprim inertia
+
+        // Reactivam coliziunile si damage-ul
+        Physics.IgnoreLayerCollision(7, 8, false);
+        if (_playerHealth != null) _playerHealth.canTakeDamage = true;
+
+        // Asteptam cooldown-ul
         yield return new WaitForSeconds(_dashCooldown);
         _canDash = true;
     }
